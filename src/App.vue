4b89d89d78d5455548cede4d3b56b40816fa0b9c
Mohammad Mehrnia <qermezkon@gmail.com>
2020-02-18 11:57:58 +0330

    Load data with axios

--- src/App.vue
+++ src/App.vue
@@ -89,17 +89,7 @@ export default {
   },
   created() {
     this.$Progress.start();
-    this.$router.beforeEach((to, from, next) => {
-      if (to.meta.progress !== undefined) {
-        let meta = to.meta.progress
-        this.$Progress.parseMeta(meta)
-      }
-      this.$Progress.start()
-      next()
-    })
-    this.$router.afterEach(() => {
-      this.$Progress.finish()
-    })
+    
   }
 };
 </script>
