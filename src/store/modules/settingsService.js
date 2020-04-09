b47bd685ecbd01402cdeddbda22444586153da51
Mohammad Mehrnia <qermezkon@gmail.com>
2020-04-09 13:21:35 +0430

    Initiale Ticket and asset menus

--- src/store/modules/settingsService.js
+++ src/store/modules/settingsService.js
@@ -15,6 +15,28 @@ const settingsService = {
           { name: "Groups",label:"", path: "/Groups", image: "Groups.png" }
         ]
       },
+      {
+        name: "TicketSettings",
+        label:"تنظیمات درخواست کار",
+        path: "/SettingsControlPanel",
+        icon: "",
+        items: [
+          { name: "Ticket Status",label:"وضعیت های درخواست", path: "/TicketStatusList", image: "" },
+          { name: "Tags",label:"برچسب ها", path: "/Tags", image: "" },
+          { name: "Category",label:"دسته بندی", path: "/Gategories", image: "" },
+          { name: "OrganizeChart",label:"چارت سازمانی", path: "/OrganizeChart", image: "" },
+        ]
+      },
+      {
+        name: "AssetSettings",
+        label:"تنظیمات اموال",
+        path: "/SettingsControlPanel",
+        icon: "",
+        items: [
+          { name: "Assets",label:"معرفی اموال", path: "/Assets", image: "" },
+          { name: "AssetsLocation",label:"موقعیت اموال", path: "/AssetsLocation", image: "" },
+        ]
+      },
     ]
   },
   mutations: {},
