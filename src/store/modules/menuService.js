543ec053f663f7ff815bffcf3ef179143b667b73
Mohammad Mehrnia <qermezkon@gmail.com>
2020-06-15 10:56:17 +0430

    60% Complete All Component

--- src/store/modules/menuService.js
+++ src/store/modules/menuService.js
@@ -3,7 +3,7 @@ const menuService = {
         menuItems: [
             {text: "داشبورد", icon: "mdi-monitor-dashboard", to: '/dashboard'},
             {text: "کارتابل", icon: "mdi-account-convert", to: "/cartabl"},
-            {text: "مدیریت اموال", icon: "mdi-desktop-classic", to: '/AssetList'},
+            {text: "کنترل اموال", icon: "mdi-desktop-classic", to: '/AssetList'},
             {text: "گزارشات", icon: "mdi-file-chart", to: '/Reports'},
             {text: "تنظیمات", icon: "mdi-cogs", to: "/settingsControlPanel"}
         ],
