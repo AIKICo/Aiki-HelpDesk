5101f66f49ebd9d10a045013b9d7af1d0f9fb77b
Mohammad Mehrnia <qermezkon@gmail.com>
2020-07-04 18:30:20 +0430

    90% Complete All Component
    Use Encrypted CompnayID

--- src/components/auth/login.vue
+++ src/components/auth/login.vue
@@ -3,60 +3,69 @@
     <v-layout align-center >
       <v-row justify="center" align="center" no-gutters>
         <v-col cols="12" md="5" lg="5" >
-          <ValidationObserver ref="observer" v-slot="{ handleSubmit, invalid }">
-            <form @submit.prevent="handleSubmit(onSubmit)" autocomplete="off">
-              <v-card class="mx-auto" outlined>
-                <v-card-title :class="$store.state.defaultColor + ' white--text'">
-                  <v-icon large color="white">mdi-handshake</v-icon>
-                  میزکار خدمات رایانه ای
-                </v-card-title>
-                <v-card-text class="mt-5">
-                  <ValidationProvider
-                    v-slot="{ errors }"
-                    name="شناسه کاربری"
-                    rules="required"
-                  >
-                    <v-text-field
-                      v-model="loginDetails.userName"
-                      label="شناسه کاربری"
-                      clearable
-                      :error-messages="errors"
-                      outlined
-                      shaped
-                      immediate
-                    ></v-text-field>
-                  </ValidationProvider>
-                  <ValidationProvider
-                    v-slot="{ errors }"
-                    name="کلمه عبور"
-                    rules="required"
-                    immediate
-                  >
-                    <v-text-field
-                      v-model="loginDetails.password"
-                      label="کلمه عبور"
-                      clearable
-                      :type="'password'"
-                      :error-messages="errors"
-                      outlined
-                      shaped
-                    ></v-text-field>
-                  </ValidationProvider>
-                </v-card-text>
+          <v-row>
+            <v-col>
+              <ValidationObserver ref="observer" v-slot="{ handleSubmit, invalid }">
+                <form @submit.prevent="handleSubmit(onSubmit)" autocomplete="off">
+                  <v-card class="mx-auto" outlined>
+                    <v-card-title :class="$store.state.defaultColor + ' white--text'">
+                      <v-icon large color="white">mdi-handshake</v-icon>
+                      میزکار خدمات رایانه ای
+                    </v-card-title>
+                    <v-card-text class="mt-5">
+                      <ValidationProvider
+                              v-slot="{ errors }"
+                              name="شناسه کاربری"
+                              rules="required"
+                      >
+                        <v-text-field
+                                v-model="loginDetails.userName"
+                                label="شناسه کاربری"
+                                clearable
+                                :error-messages="errors"
+                                outlined
+                                shaped
+                                immediate
+                        ></v-text-field>
+                      </ValidationProvider>
+                      <ValidationProvider
+                              v-slot="{ errors }"
+                              name="کلمه عبور"
+                              rules="required"
+                              immediate
+                      >
+                        <v-text-field
+                                v-model="loginDetails.password"
+                                label="کلمه عبور"
+                                clearable
+                                :type="'password'"
+                                :error-messages="errors"
+                                outlined
+                                shaped
+                        ></v-text-field>
+                      </ValidationProvider>
+                    </v-card-text>
 
-                <v-card-actions>
-                  <v-btn
-                    :color="$store.state.defaultColor + ' darken-1'"
-                    :disabled="invalid"
-                    value="login"
-                    type="submit"
-                    text
-                    >ورود به سامانه</v-btn
-                  >
-                </v-card-actions>
-              </v-card>
-            </form>
-          </ValidationObserver>
+                    <v-card-actions>
+                      <v-btn
+                              :color="$store.state.defaultColor + ' darken-1'"
+                              :disabled="invalid"
+                              value="login"
+                              type="submit"
+                              text
+                      >ورود به سامانه</v-btn
+                      >
+                    </v-card-actions>
+                  </v-card>
+                </form>
+              </ValidationObserver>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-btn x-large value="register" class="col-12 info">ثبت نام</v-btn>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-layout>
