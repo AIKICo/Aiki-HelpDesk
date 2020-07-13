9889376449f0f6604301cb7b93d4c48c7445646f
Mohammad Mehrnia <qermezkon@gmail.com>
2020-07-13 07:52:44 +0430

    Add Customer Title Group For Cartabl

--- src/store/modules/menuService.js
+++ src/store/modules/menuService.js
@@ -5,7 +5,6 @@ const menuService = {
             {text: "کارتابل", icon: "mdi-briefcase-check", to: "/cartabl", role: "admin,user"},
             {text: "آرشیو درخواست ها", icon: "mdi-archive", to: "/TicketsArchive", role: "admin,user"},
             {text: "کنترل اموال", icon: "mdi-desktop-classic", to: '/AssetList', role: "admin,user"},
-            {text: "گزارشات", icon: "mdi-file-chart", to: '/Reports', role: "admin,user"},
             {text: "تنظیمات", icon: "mdi-cogs", to: "/settingsControlPanel", role: "admin"}
         ],
     }
