03aefcc54025b82900406ebbefbddee4cd9188a9
Mohammad Mehrnia <qermezkon@gmail.com>
2020-08-16 12:06:12 +0430

    Add backup user Permission

--- src/store/modules/menuService.js
+++ src/store/modules/menuService.js
@@ -1,11 +1,11 @@
 const menuService = {
     state: {
         menuItems: [
-            {text: "داشبورد", icon: "mdi-monitor-dashboard", to: '/dashboard', role: "admin,user"},
-            {text: "کارتابل", icon: "mdi-briefcase-check", to: "/cartabl", role: "admin,user"},
-            {text: "آرشیو درخواست ها", icon: "mdi-archive", to: "/TicketsArchive", role: "admin,user"},
-            {text: "کنترل اموال", icon: "mdi-desktop-classic", to: '/AssetList', role: "admin,user"},
-            {text: "تنظیمات", icon: "mdi-cogs", to: "/settingsControlPanel", role: "admin,user"}
+            {text: "داشبورد", icon: "mdi-monitor-dashboard", to: '/dashboard', role: "admin,user,backupuser"},
+            {text: "کارتابل", icon: "mdi-briefcase-check", to: "/cartabl", role: "admin,user,backupuser"},
+            {text: "آرشیو درخواست ها", icon: "mdi-archive", to: "/TicketsArchive", role: "admin,user,backupuser"},
+            {text: "کنترل اموال", icon: "mdi-desktop-classic", to: '/AssetList', role: "admin,backupuser"},
+            {text: "تنظیمات", icon: "mdi-cogs", to: "/settingsControlPanel", role: "admin,backupuser"}
         ],
     }
 };
